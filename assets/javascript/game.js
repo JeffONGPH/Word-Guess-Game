4a2601999b6afd8ddbfb20b64ed0e4ab04c07d4e
Jeff Ong <JeffOng@PEIKs-MacBook-Pro.local>
2018-06-08 14:00:25 -0400

    Basic javascript completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,77 @@
 //variables//
 
-var wordList = ["ACDC","guitar","drums"];
+var wordList = ["ACDC", "guitar", "drums"];
 
-var userInput;
-
-var compWord = wordList[Math.floor(Math.random()*wordList.length)];
-console.log(compWord);
-
-var alphabets = compWord.toUpperCase().split("");
-console.log(alphabets);
-
-var guesses = [];
-
-var display = [];
-console.log("display =" + display)
-
+var compWord;
+var alphabets;
 var wins = 0;
-
 var loses = 0;
-
+var userInput;
 var guessesLeft = 9;
+var guesses = [];
+var display = [];
 
 
-//Start Game with dashes//
+
+//Start running a new game//
 function start() {
-    
-    var userInput;
-
-    var compWord = wordList[Math.floor(Math.random()*wordList.length)];
+    compWord = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(compWord);
-    
-    var alphabets = compWord.toUpperCase().split("");
+
+    alphabets = compWord.toUpperCase().split("");
     console.log(alphabets);
-    
-    var guesses = [];
-    
-    var display = [];
-    console.log("display =" + display)
-    
 
-    for (i=0; i<alphabets.length; i++){
-      display.push("-");
-        console.log(display) 
+    guessesLeft = 9;
+    guesses = [];
+    display = [];
+
+    for (i = 0; i < alphabets.length; i++) {
+        display.push("-");
+        console.log(display)
     }
-
     document.getElementById("display").innerHTML = display.join("");
 };
 
 
 //User inputs and processing//
-document.onkeyup = function (event) {
-    var userInput = String.fromCharCode(event.keyCode).toUpperCase();
-    console.log(userInput);
-    guesses.push(userInput);
-    console.log("Guesses made = " + guesses);
+function processing() {
 
-    for (var i=0; i<alphabets.length; i++){
-        if (userInput === alphabets[i]) { 
-            display[i] = userInput;
-            console.log("display = " + display);
-            document.getElementById("display").innerHTML = display.join("");
-           }
+    document.onkeyup = function (event) {
+        userInput = String.fromCharCode(event.keyCode).toUpperCase();
+        console.log(userInput);
+        guesses.push(userInput);
+        console.log("Guesses made = " + guesses);
+
+        for (var i = 0; i < alphabets.length; i++) {
+            if (userInput === alphabets[i]) {
+                display[i] = userInput;
+                console.log("display = " + display);
+                document.getElementById("display").innerHTML = display.join("");
+            }
         }
 
-        if (alphabets.includes(userInput) === false ){
+        if (alphabets.includes(userInput) === false) {
             loses++;
             console.log("loses = " + loses);
             guessesLeft--;
-            console.log("guessesLeft = "  + guessesLeft);
-           }
+            console.log("guessesLeft = " + guessesLeft);
+        }
 
-           if (display.toString() == alphabets.toString()){
+        if (display.toString() == alphabets.toString()) {
             wins++;
             console.log("wins = " + wins);
-            alert("You guessed the word " + compWord +  " !")
+            alert("You guessed the word " + compWord.toUpperCase() + " !")
             confirm("Try again")
-            compWord = wordList[Math.floor(Math.random()*wordList.length)];
             start()
-           }
-    
-       
-        if (guessesLeft <= 0) {
-         alert("Game Over" + "\n" + "You ran out of tries" + "\n" + "Click ok to try again");
-         compWord = wordList[Math.floor(Math.random()*wordList.length)];
-         console.log(compWord);
-         guessesLeft = 9;
-         console.log(guessesLeft);
-     }
-   
-    };
+        }
 
+        if (guessesLeft <= 0) {
+            alert("Game Over" + "\n" + "You ran out of tries" + "\n" + "Click ok to try again");
+            start()
+        }
+    }
+
+};
+
+start()
+processing()
